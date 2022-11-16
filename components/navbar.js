1d35fba3b7b34117786add7d65b5bf0395af3854
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-11-15 19:35:28 -0800

    add book

--- components/navbar.js
+++ components/navbar.js
@@ -6,7 +6,8 @@ export default function Navbar({ children }) {
     <div className={styles.navbar}>
       <ul>
        <li className={styles.left}><Link href="/">Home</Link></li>
-       <li className={styles.left}><Link href="/schedule">Schedule</Link></li>
+       <li className={styles.left}><Link target="_top" href="https://squareup.com/appointments/book/fav37xmfpk4rh4/L66805DQ6QAN4/start" rel="nofollow">Book Now
+       </Link></li>
        <li className={styles.right}><Link href="tel:8056382432">Call 805-638-2432</Link></li>
       </ul>
     </div>
