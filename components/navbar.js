8ccf53f91fcc12e11319dbe79b84fba025dc1359
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-11-18 06:37:16 -0800

    added favicon

--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,12 @@
 import styles from '../styles/Home.module.css'
+import logo from '../public/autoglass.png'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Navbar({ children }) {
   return (
     <div className={styles.navbar}>
       <ul>
-       <li className={styles.left}><Link href="/">Home</Link></li>
        <li className={styles.left}><Link target="_top" href="https://squareup.com/appointments/book/fav37xmfpk4rh4/L66805DQ6QAN4/start" rel="nofollow">Book Now
        </Link></li>
        <li className={styles.right}><Link href="tel:8056382432">Call 805-638-2432</Link></li>
